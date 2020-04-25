a00683dc2d43d1e4f98f76dea5d4e37f89fef288
Abas Hersi <55505133+abis206@users.noreply.github.com>
2020-04-25 15:18:09 -0700

    Update Lift.java

--- src/main/java/frc/team8051/subsystems/Lift.java
+++ src/main/java/frc/team8051/subsystems/Lift.java
@@ -4,7 +4,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import frc.team8051.Robot;
 
 public class Lift {
-    public static void periodic(){
+    public static void periodic() {
         double maximumOverDrive = 70;
         final WPI_VictorSPX shooterMotor = new WPI_VictorSPX(0);
         // if(Robot.getInstance().getOI().joystick.getRawAxis(5) > 0){
